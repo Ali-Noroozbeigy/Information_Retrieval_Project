c7b8ec3b736359a891611d39695c9f696e5f5a9e
Ali Noroozbeigy <ali.noroozbeigy@gmail.com>
2023-04-12 23:03:23 +0330

    add some refactor to index construction

--- index_construct.py
+++ index_construct.py
@@ -26,23 +26,25 @@ class PositionalIndex:
 
     def add_to_index(self, tokens, doc_id):
         for i, token in enumerate(tokens):
-            if token in self.pos_index:
+            if token in self.pos_index: # token is available in index
                 self.pos_index[token]['total_freq'] += 1
 
                 doc_found = False
 
-                for posting in self.pos_index[token]['postings']:
-                    if doc_id == posting['doc_id']:
-                        posting['in_doc_freq'] += 1
-                        posting['positions'].append(i)
-                        doc_found = True
-                        break
+                # token is ready, we check if it is belong to the current doc_id which is being processed
+                # we need to update the posting ny adding the new position and increase freq
+                last_posting = self.pos_index[token]['postings'][-1]
+                if doc_id == last_posting['doc_id']:
+                    last_posting['in_doc_freq'] += 1
+                    last_posting['positions'].append(i)
 
-                if not doc_found:
+                # token is ready, but it is not from the last doc_id, so we need to add a new posting
+                else:
                     self.pos_index[token]['postings'].append({"doc_id": doc_id,
                                                               "in_doc_freq": 1,
                                                               "positions": [i]})
 
+            # token is totally new, we need to add it and set initial values
             else:
                 self.pos_index[token] = {"total_freq": 1, "postings": [{"doc_id": doc_id,
                                                                         "in_doc_freq": 1,
