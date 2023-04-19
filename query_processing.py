081e81efe08af6a4675a6db2683d393f7671e37b
Ali Noroozbeigy <ali.noroozbeigy@gmail.com>
2023-04-19 11:15:17 +0330

    add some refactor

--- query_processing.py
+++ query_processing.py
@@ -1,38 +1,41 @@
 from preprocessing import Preprocessing
 
 
-query = '"جواد خیابانی" ! گزارشگر فوتبال'
+def parse_query():
+    tokens = Preprocessing.tokenize(Preprocessing.normalize(query))
 
-tokens = Preprocessing.tokenize(Preprocessing.normalize(query))
+    length_of_tokens = len(tokens)
 
-length_of_tokens = len(tokens)
+    i = 0
+    while i < length_of_tokens:
+        if tokens[i] == "«":
+            i += 1
+            start = i
+
+            while tokens[i] != "»":
+                i += 1
+
+            end = i
+
+            positional_queries.append(Preprocessing.remove_stopwords(Preprocessing.stem_tokens(tokens[start:end])))
+            i += 1
+
+        elif tokens[i] == '!':
+            i += 1
+            and_not_queries.append(Preprocessing.remove_stopwords(Preprocessing.stem_tokens(tokens[i:i+1]))[0])
+            i += 1
+
+        else:
+            and_queries.append(Preprocessing.remove_stopwords(Preprocessing.stem_tokens(tokens[i:i+1]))[0])
+            i += 1
+
 
 positional_queries = []
 and_not_queries = []
 and_queries = []
 
-i = 0
-while i < length_of_tokens:
-    if tokens[i] == "«":
-        i += 1
-        start = i
-
-        while tokens[i] != "»":
-            i += 1
-
-        end = i
-
-        positional_queries.append(Preprocessing.remove_stopwords(Preprocessing.stem_tokens(tokens[start:end])))
-        i += 1
-
-    elif tokens[i] == '!':
-        i += 1
-        and_not_queries.append(Preprocessing.remove_stopwords(Preprocessing.stem_tokens(tokens[i:i+1]))[0])
-        i += 1
-
-    else:
-        and_queries.append(Preprocessing.remove_stopwords(Preprocessing.stem_tokens(tokens[i:i+1]))[0])
-        i += 1
+query = '"جواد خیابانی" ! گزارشگر فوتبال'
+parse_query()
 
 print(positional_queries)
 print(and_not_queries)
