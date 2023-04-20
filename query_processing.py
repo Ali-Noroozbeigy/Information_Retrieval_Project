bbc5674aec70fca7149919bdb974ac93e2662dfa
Ali Noroozbeigy <ali.noroozbeigy@gmail.com>
2023-04-20 10:04:46 +0330

    add positional intersect

--- query_processing.py
+++ query_processing.py
@@ -1,3 +1,5 @@
+import json
+
 from preprocessing import Preprocessing
 
 
@@ -30,13 +32,47 @@ def parse_query():
             i += 1
 
 
+def positional_intersect(p1, p2):
+    """pi is the postings list of ith word"""
+
+    answer = []
+
+    i = j = 0
+
+    while i != len(p1) and j != len(p2):
+        if p1[i]['doc_id'] == p2[j]['doc_id']:
+
+            positions_1 = p1[i]['positions']
+            positions_2 = p2[j]['positions']
+
+            pp1 = pp2 = 0
+            while pp1 != len(positions_1) and pp2 != len(positions_2):
+                if positions_1[pp1] == positions_2[pp2] - 1:
+                    answer.append({'doc_id': p1[i]['doc_id'], 'position': positions_1[pp1]})
+                    pp1 += 1
+                    pp2 += 1
+                elif positions_1[pp1] < positions_2[pp2] - 1:
+                    pp1 += 1
+                else:
+                    pp2 += 1
+            i += 1
+            j += 1
+        elif p1[i]['doc_id'] < p2[j]['doc_id']:
+            i += 1
+        else:
+            j += 1
+    return answer
+
+
 positional_queries = []
 and_not_queries = []
 and_queries = []
 
-query = '"جواد خیابانی" ! گزارشگر فوتبال'
+query = '"وزیر اقتصاد"'
 parse_query()
 
-print(positional_queries)
-print(and_not_queries)
-print(and_queries)
+with open('./positional_index.json', 'r', encoding="utf-8") as data_file:
+    data = json.load(data_file)
+
+ans = positional_intersect(data[positional_queries[0][0]]['postings'], data[positional_queries[0][1]]['postings'])
+print(ans)
